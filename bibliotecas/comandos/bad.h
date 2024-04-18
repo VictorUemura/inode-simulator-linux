4adcf6bc708134140a250436513adb8df3931982
Iagho Slavieiro <133013654+iagho-slaviero@users.noreply.github.com>
2024-04-17 23:43:56 -0300

    feat: criacao do comando bad

--- bibliotecas/comandos/bad.h
+++ bibliotecas/comandos/bad.h
@@ -1,1 +1,12 @@
+int pegaNumero(char input[]) {
+    int numero;
 
+    sscanf(input, "%*s %d", &numero);
+    
+    return numero;
+}
+
+void comandoBad(char comando[], Bloco disco[]){
+	int numBloco = pegaNumero(comando);
+	printf("Bloco: %d", numBloco);
+}
