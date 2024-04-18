0d51e0a5d76aaab0b931b7368d46a406910d9b2d
VictorUemura <victoruemura04@hotmail.com>
2024-04-18 08:12:15 -0300

    feat: implementacao do mkdir atraves de endereco direto

--- bibliotecas/comandos/mkdir.h
+++ bibliotecas/comandos/mkdir.h
@@ -1,6 +1,11 @@
 // Função que implementa o comando mkdir
 void comandoMkdir(Bloco disco[], char comando[], char caminho[], int endRoot) {
-    // verifica se o comando mkdir contem argumentos
+    if(!haBlocosLivres(disco)) {
+		printf("-bash: mkdir: No free blocks.\n");
+        return;
+	}
+	
+	// verifica se o comando mkdir contem argumentos
     if (strlen(comando) == 0) {
         printf("-bash: mkdir: The 'mkdir' command requires an argument (directory name to be created).\n");
         return;
@@ -19,8 +24,64 @@ void comandoMkdir(Bloco disco[], char comando[], char caminho[], int endRoot) {
     strcpy(nomeDiretorio, token);
     
 	// andar pelo caminho ate o inode do ultimo endereco
+	int endAux = endRoot;
 	char *token2 = strtok(caminho, "/");
-	printf("token2 %s\n", token2);
-	while(token2 != )    
+	// implementar o andar pelos caminhos
+	
+	// busca entrada livre
+	int endDiretorio;
+	int endNovaEntrada;
+	
+	// se flag 0 nao encontrou se flag 1 encontrou entrada se flag 2 cria nova entrada
+	int flag = 0;
+	int endIndexDireto;
+	int flagEntrada;
+	int endEntrada;
+	int endIndex;
+	
+	for(int i = 0; i < 5 && flag == 0; i++) {
+		if(disco[endAux].inode.direto[i] == -1) {
+			flag = 2;
+			endIndexDireto = i;
+		}
+				
+		else {
+			flag = 1;
+			// busca dentro das entradas para ver se existe posicao livre
+			flagEntrada = 0;
+			endEntrada = disco[endAux].inode.direto[i];
+			
+			for(int j = 0; j < 10 && flagEntrada == 0; j++) {
+				if(disco[endEntrada].diretorio.entradas[j].inodeEndereco == -1) {
+					flagEntrada = 1;
+					endIndex = j;
+				}
+			}
+			
+			if(flagEntrada == 0)
+				flag = 0;
+		}
+	}
+	
+	// tem uma entrada direta disponivel
+	if(flag == 1) {
+		int blocoLivre = obterBlocoLivre(disco);
+		disco[blocoLivre] = initBlocoInode();
+		disco[blocoLivre].inode = criaModeloInode(nomeDiretorio, 'd', "rw-r--r--");
+		
+		disco[endEntrada].diretorio.entradas[endIndex].inodeEndereco = blocoLivre;
+		strcpy(disco[endEntrada].diretorio.entradas[endIndex].nome, nomeDiretorio);
+	}
+	if(flag == 2) {
+		int blocoLivre = obterBlocoLivre(disco);
+		disco[blocoLivre] = initBlocoDiretorio();
+		
+		int blocoLivreNovo = obterBlocoLivre(disco);
+		disco[blocoLivre].diretorio.entradas[0].inodeEndereco = blocoLivreNovo;
+		strcpy(disco[blocoLivre].diretorio.entradas[0].nome, nomeDiretorio);
+	
+		disco[blocoLivreNovo] = initBlocoInode();
+		disco[blocoLivreNovo].inode = criaModeloInode(nomeDiretorio, 'd', "rw-r--r--");
+	}
 }
 
